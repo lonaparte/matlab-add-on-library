5ec108876409f24ea50912531c9ac22dfdcf9dd3
lonaparte <chenglidev@outlook.com>
2018-06-12 00:59:11 +0800

    0612

--- myIFFT.m
+++ myIFFT.m
@@ -1,15 +1,21 @@
-function [yef,tt] = myIFFT(F,fsample,T)
+function [yef,tt] = myIFFT(F,fx,t)
 % Calculate the IFFT in a normal used form.
+% Usage:
+%   [yef,tt] = myIFFT(F,fx,t)
 % Input:
 %   F: the spectra of signal
-%   fsample: the sample frequency in the spectra
-%   T: the time range [-T/2,T/2] that we want in time domain
+%   fx: the correspond frequency series
+%   t: the time range [-t,t] that we want in time domain
 % Output:
 %   yef,tt: to plot the result
+% Modified and Used by Lonaparte CHENG
+% Website: https://github.com/lonaparte/matlab-add-on-library
+t = t*2;
+fsample = (length(F)-1)/fx(end)/2;
 range = (length(F)-1)/fsample;
 y = ifft(F*range);
-dt = T/length(y);
-tt = (0:dt:dt*(length(y)-1)) - T/2;
+dt = t/length(y);
+tt = (0:dt:dt*(length(y)-1)) - t/2;
 yef = ifftshift(abs(y));
 end
 
