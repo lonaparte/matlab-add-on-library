5f75bec504babdb75006b13ebf5b0eef3c0e655b
lonaparte <chenglidev@outlook.com>
2018-06-12 09:15:48 +0800

    0612

--- myFFT.m
+++ myFFT.m
@@ -1,20 +1,29 @@
-function [F,fx] = myFFT(x,t,N)
+function [F,fx] = myFFT(varargin)
 % Calculate the FFT in a normal used form.
 % Usage:
 %   [F,fx] = myFFT(x,t,N)
+%   [F,fx] = myFFT(x,t,N,'norm')
 % Input:
 %   x: the signal in discrete time domain
 %   t: the time serie in time domain. The effect element is t(end).
 %   N: 2^N is the FFT point number to realize 
+%   'norm': to normalize the output spectrum
 % Output:
 %   F: the normalized value of FFT
 %   fx: the correspond frequence
 % Modified and Used by Lonaparte CHENG
 % Website: https://github.com/lonaparte/matlab-add-on-library
-    tr = t(end);
-    fs = (length(x)-1)/2/tr;
-    df = fs/2^N;
-    fx = (0:df:df*(2^N-1)) - fs/2;
-    value = abs(fft(x,2^N))/fs;
+x = varargin{1};
+t = varargin{2};
+N = varargin{3};
+tr = t(end);
+fs = (length(x)-1)/2/tr;
+df = fs/2^N;
+fx = (0:df:df*(2^N-1)) - fs/2;
+if nargin == 3
+    value = abs(fft(x,2^N))/2^N;
+    F = fftshift(value);
+elseif varargin{4} == 'norm'
+    value = abs(fft(x,2^N))/max(abs(fft(x,2^N)));
     F = fftshift(value);
 end
--- myIFFT.m
+++ myIFFT.m
@@ -1,19 +1,17 @@
-function [yef,tt] = myIFFT(F,fx,t)
+function [yef,tt] = myIFFT(F,fx,N)
 % Calculate the IFFT in a normal used form.
 % Usage:
-%   [yef,tt] = myIFFT(F,fx,t)
+%   [yef,tt] = myIFFT(F,fx,N)
 % Input:
 %   F: the spectra of signal
-%   fx: the correspond frequency series
-%   t: the time range [-t,t] that we want in time domain
+%   fx: the correspond frequency series, the effect element is fx(end)
+%   N: 2^N is the FFT point number to realize
 % Output:
 %   yef,tt: to plot the result
 % Modified and Used by Lonaparte CHENG
 % Website: https://github.com/lonaparte/matlab-add-on-library
-t = t*2;
-fsample = (length(F)-1)/fx(end)/2;
-range = (length(F)-1)/fsample;
-y = ifft(F*range);
+y = ifft(F*2^N,2^N);
+t = (length(y)-1)/2/fx(end);
 dt = t/length(y);
 tt = (0:dt:dt*(length(y)-1)) - t/2;
 yef = ifftshift(abs(y));
