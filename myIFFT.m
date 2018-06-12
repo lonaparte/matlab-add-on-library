f4c492a67dcdfe31e1ba942d2892d69244b55986
lonaparte <chenglidev@outlook.com>
2018-06-12 14:21:37 +0800

    0612

--- myIFFT.m
+++ myIFFT.m
@@ -1,19 +1,27 @@
-function [yef,tt] = myIFFT(F,fx,N)
+function [yef,tt] = myIFFT(varargin)
 % Calculate the IFFT in a normal used form.
 % Usage:
 %   [yef,tt] = myIFFT(F,fx,N)
+%   [yef,tt] = myIFFT(F,fx,N,'notabs')
 % Input:
 %   F: the spectra of signal
 %   fx: the correspond frequency series, the effect element is fx(end)
 %   N: N is the FFT point number to realize
+%   'notabs': do not calculate the absolute value of the result
 % Output:
 %   yef,tt: to plot the result
 % Modified and Used by Lonaparte CHENG
 % Website: https://github.com/lonaparte/matlab-add-on-library
+F = varargin{1};
+fx = varargin{2};
+N = varargin{3};
 y = ifft(F*N,N);
 t = (length(y)-1)/2/fx(end);
 dt = t/length(y);
 tt = (0:dt:dt*(length(y)-1)) - t/2;
-yef = ifftshift(abs(y));
+if nargin == 3
+    yef = ifftshift(abs(y));
+elseif varargin{4} == 'notabs'
+    yef = ifftshift(y);
 end
 
